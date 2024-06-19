7cc4d235c6dd92b04b917ec8d5003018c6b059a5
Tiago Fedel <tiagoregis@yahoo.com.br>
2024-06-18 21:23:05 -0300

    teste de operação

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,8 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        int numero = -3;
+        System.out.println(- numero);
+        System.out.println(+ numero);
     }
 }
