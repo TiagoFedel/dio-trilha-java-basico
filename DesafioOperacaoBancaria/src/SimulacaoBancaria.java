f4c61f3a948e83721b6a6b972830ef64be482473
Tiago Fedel <tiagoregis@yahoo.com.br>
2024-07-28 10:55:26 -0300

    mais desafios

---
+++ DesafioOperacaoBancaria/src/SimulacaoBancaria.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class SimulacaoBancaria {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double saldo = 0;
+        boolean continuar = true;
+
+        while (continuar) {
+            System.out.println("Digite a opcao: ");
+            int opcao = scanner.nextInt();
+
+            switch (opcao) {
+                case 1:
+                    // TODO: Ler o valor a ser depositado e atualizar/imprimir o saldo.
+                    System.out.println("Digite o valor a ser depositado:");
+                    double deposito = scanner.nextDouble();
+                    saldo = saldo + deposito;
+                    System.out.println("Saldo atual: " + saldo);
+                    break;
+                case 2:
+                    // TODO: Ler o valor a ser sacado e verificar/imprimir se há saldo suficiente.
+                    System.out.println("Digite o valor a ser sacado:");
+                    double sacar = scanner.nextDouble();
+                    if (sacar <= saldo) {
+                        saldo = saldo - sacar;
+                        System.out.println("Saldo atual: " + saldo);
+                    } else
+                        System.out.println("Saldo insuficiente");
+                    break;
+                case 3:
+                    // TODO: Exibir o saldo atual da conta.
+                    System.out.println("Saldo atual: " + saldo);
+                    break;
+                case 0:
+                    System.out.println("Programa encerrado.");
+                    continuar = false;  // Atualiza a variável de controle para encerrar o loop
+                    break;
+                default:
+                    System.out.println("Opção inválida. Tente novamente.");
+            }
+        }
+        scanner.close();
+    }
+}
